918b2f513312fa946e82dc09e99972a67a6b0029
Russell Taff <rtaff2012@gmail.com>
2014-01-15 14:23:29 -0500

    final commit

--- trailerPark/app/models/movie.rb
+++ trailerPark/app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
-has_and_belongs_to_many :users
+  has_and_belongs_to_many :users
 end
